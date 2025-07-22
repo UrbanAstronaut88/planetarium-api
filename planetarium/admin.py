bb1ad34dc4f8d53f1122497555def43c018cfb98
BondarenkoKirill <bku089@gmail.com>
2025-07-22 15:38:42 +0300

    Add ShowSession, Reservation, and Ticket models

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import (PlanetariumDome,
                      AstronomyShow,
-                     ShowTheme
+                     ShowTheme,
+                     ShowSession,
+                     Reservation,
+                     Ticket,
                      )
 
 
 admin.site.register(PlanetariumDome)
 admin.site.register(AstronomyShow)
 admin.site.register(ShowTheme)
+admin.site.register(ShowSession)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
--- planetarium/models.py
+++ planetarium/models.py
@@ -26,3 +26,49 @@ class ShowTheme(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        AstronomyShow,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.astronomy_show.title} at {self.show_time}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservations")
+
+    def __str__(self):
+        return f"Reservation #{self.id} by {self.user}"
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    class Meta:
+        unique_together = ("row", "seat", "show_session")
+
+    def __str__(self):
+        return f"Ticket: row {self.row}, seat {self.seat}, for session {self.show_session.id}"
