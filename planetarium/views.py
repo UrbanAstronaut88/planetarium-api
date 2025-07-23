0926247befc816ca1b3df407e1e9a5e46e8abab0
BondarenkoKirill <bku089@gmail.com>
2025-07-23 17:28:59 +0300

    Add custom endpoint to get available seats for ShowSession

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from planetarium.models import (PlanetariumDome,
                                 AstronomyShow,
                                 ShowTheme,
@@ -38,6 +41,30 @@ class ShowSessionViewSet(viewsets.ModelViewSet):
     serializer_class = ShowSessionSerializer
     permission_classes = [permissions.AllowAny]
 
+    @action(detail=True, methods=["get"], url_path="available-seats")
+    def available_seats(self, request, pk=None):
+        session = self.get_object()
+        dome = session.planetarium_dome
+
+        # в с е   в о з м о ж н ы е   м е с т а
+        all_seats = set(
+            (row, seat)
+            for row in range(1, dome.rows + 1)
+            for seat in range(1, dome.seats_in_row + 1)
+        )
+
+        # з а н я т ы е   м е с т а   п о   б и л е т а м
+        taken_seats = set(
+            session.tickets.values_list("row", "seat")
+        )
+
+        # с в о б о д н ы е  -  э т о   р а з н о с т ь
+        available = sorted(list(all_seats - taken_seats))
+
+        return Response({
+            "show_session": session.id,
+            "available_seats": available,
+        })
 
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
